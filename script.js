daf17553f0dd23db7d677d00d4a2b970d9b68e6c
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-26 13:32:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj = {};
+	for(let i = 0; i < str.length; i++){
+		if(obj[str[i]] == undefined){
+			obj[str[i]] = 1;
+		}
+		else{
+			obj[str[i]]++;
+		}
+	}
+
+	for(let x  in obj){
+		if(obj[x] == 1){
+			return x; 
+		}
+	}
 }
-const input = prompt("Enter a string");
+
+const input = prompt("Enter a string"); 
 alert(firstNonRepeatedChar(input)); 
